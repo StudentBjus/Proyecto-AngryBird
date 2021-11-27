76730bd047f50aa653fc7daa5aac52afc7b85535
StudentBjus <90108320+StudentBjus@users.noreply.github.com>
2021-11-26 20:53:20 -0600

    Add files via upload

---
+++ Slingshot.js
@@ -0,0 +1,29 @@
+class Slingshot 
+{
+    constructor(bodyA,pointB){
+      var options=
+      {
+bodyA:bodyA,
+pointB:pointB,
+stiffness: 0.04,
+lenth: 10
+
+      }
+      this.sling= Constraint.create(options);
+      World.add(world,this.sling);
+    }
+     fly()
+     {
+     this.sling.bodyA=null;
+
+
+
+     }
+     
+     display() {
+      var pointA=this.sling.bodyA.position;
+      var pointB=this.pointB;
+      strokeWeight(3);
+      line(pointA.x, pointA.y, pointB.x, pointB.y);
+    }
+  }
